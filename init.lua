6db33848b28bdbbf6ccc992d799477c23ca86bf7
Almaz <almazshai@gmail.com>
2026-01-30 10:25:06 +0100

    Disable tag for nvim-surrond

--- init.lua
+++ init.lua
@@ -22,7 +22,7 @@ require('packer').startup(function(use)
     -- surround package
     use({
        "kylechui/nvim-surround",
-          tag = "fcfa7e02323d57bfacc3a141f8a74498e1522064", -- Use for stability; omit to use `main` branch for the latest features
+          -- tag = "fcfa7e02323d57bfacc3a141f8a74498e1522064", -- Use for stability; omit to use `main` branch for the latest features
           config = function()
               require("nvim-surround").setup({
                   -- Configuration here, or leave empty to use defaults
