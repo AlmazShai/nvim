90571a5e370c279559a36338e12167e6b9ad6dac
Almaz <almazshai@gmail.com>
2026-02-02 15:14:56 +0100

    Save on jk

--- lua/user/terminal_keymaps.lua
+++ lua/user/terminal_keymaps.lua
@@ -4,7 +4,7 @@ local opts = { noremap = true, silent = true }
 
 -- Insert mode: 'jj' to exit insert mode
 keymap("i", "jj", "<Esc>")
-keymap("i", "jk", "<Esc>")
+keymap("i", "jk", "<Esc>:w<CR>")
 
 -- Normal mode: <leader>w to save
 keymap("n", "<leader>w", ":w<CR>")
