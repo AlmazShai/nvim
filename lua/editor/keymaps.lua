0f916c11c2646f248f59c6e061191344ac5c129d
Almaz <almazshai@gmail.com>
2026-02-19 14:06:53 +0100

    Fix typo

--- lua/editor/keymaps.lua
+++ lua/editor/keymaps.lua
@@ -12,7 +12,7 @@ keymap({"n", "v"}, "<leader>cn", "<cmd>lua require('vscode').action('notificatio
 keymap({"n", "v"}, "<leader>cp", "<cmd>lua require('vscode').action('workbench.action.showCommands')<CR>")
 keymap({"n", "v"}, "<leader>pr", "<cmd>lua require('vscode').action('code-runner.run')<CR>")
 keymap({"n", "v"}, "<leader>fd", "<cmd>lua require('vscode').action('workbench.action.formatDocument')<CR>")
-keymap({"n", "v"}, "<leader>w", "<cmd>lua require('vscode').action('workbench.actionk.files.save')<CR>")
+keymap({"n", "v"}, "<leader>w", "<cmd>lua require('vscode').action('workbench.action.files.save')<CR>")
 keymap({"n", "v"}, "<leader>q", "<cmd>lua require('vscode').action('workbench.action.closeActiveEditor')<CR>")
 keymap({"n", "v"}, "<leader>o", "<cmd>lua require('vscode').action('clangd.switchheadersource')<CR>")
 
